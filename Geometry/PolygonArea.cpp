8bf93ab02405cffcabf179fc7bdd8e460a485962
vktalws <victorfalvess22@gmail.com>
2026-02-24 17:48:11 -0300

    Polygon Area feita

---
+++ Geometry/PolygonArea.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define vi vector<int>
+#define vll vector<ll>
+#define vpii vector<pii>
+#define vpll vector<pll>
+
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((int)(x).size())
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+
+#define rep(i, a, b) for (int i = (a); i < (b); i++)
+#define rep0(i, a) rep(i, 0, a)
+#define rep1(i, a) rep(i, 1, a + 1)
+#define repr(i, a, b) for (int i = (b) - 1; i >= (a); i--)
+
+#define endl '\n'
+#define fastio ios_base::sync_with_stdio(false), cin.tie(nullptr)
+
+void solve() {
+	int n; cin >> n;
+    vll x(n), y(n);
+    rep0(i, n) {
+        cin >> x[i] >> y[i];
+    }
+    ll out = 0;
+    //ja estao ordenados
+    rep0(i, n) {
+        int j = (i + 1) % n;
+        out += x[i] * y[j];
+        out -= x[j] * y[i];
+    }
+    cout << abs(out) << endl;
+}
+
+int main() {
+    fastio;
+    int t = 1;
+    //cin >> t;
+    while(t--) {
+	    solve();
+    }
+
+    return 0;
+}
+
