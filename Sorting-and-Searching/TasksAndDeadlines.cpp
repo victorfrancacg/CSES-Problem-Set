250f290f9914561513f9b3b9969ca3ed5e7d1fb1
vktalws <victorfalvess22@gmail.com>
2026-07-07 12:14:01 -0300

    feat(sorting): tasks and deadlines

---
+++ Sorting-and-Searching/TasksAndDeadlines.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define vi vector<int>
+#define vll vector<ll>
+#define vpii vector<pii>
+#define vpll vector<pll>
+
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((int)(x).size())
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+
+#define rep(i, a, b) for (int i = (a); i < (b); i++)
+#define rep0(i, a) rep(i, 0, a)
+#define rep1(i, a) rep(i, 1, a + 1)
+#define repr(i, a, b) for (int i = (b) - 1; i >= (a); i--)
+
+#define endl '\n'
+#define fastio ios_base::sync_with_stdio(false), cin.tie(nullptr)
+
+void solve() {
+	ll out = 0;
+    int n; cin >> n;
+    ll sdur = 0, disc = 0;
+    vi dur(n);
+    rep0(i, n) {
+        int a, d; cin >> a >> d;
+        sdur += a;
+        out += d;
+        dur[i] = a;
+    }
+
+    sort(all(dur));
+
+    for(int i = n - 1; i >= 0; i--) {
+        out -= (sdur - disc);
+        disc += dur[i];
+    }
+
+    cout << out << endl;
+}
+
+int main() {
+    fastio;
+    int t = 1;
+    //cin >> t;
+    while(t--) {
+	    solve();
+    }
+
+    return 0;
+}
+
