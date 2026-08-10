eb6bc2176c7eeff034749d1dee802f10a5711f59
vktalws <victorfalvess22@gmail.com>
2026-08-10 18:50:59 -0300

    feat(sorting & searching): nearest smaller values

---
+++ Sorting-and-Searching/NearestSmallerValues.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define vi vector<int>
+#define vll vector<ll>
+#define vpii vector<pii>
+#define vpll vector<pll>
+
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((int)(x).size())
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+
+#define rep(i, a, b) for (int i = (a); i < (b); i++)
+#define rep0(i, a) rep(i, 0, a)
+
+#define fastio ios_base::sync_with_stdio(false), cin.tie(nullptr)
+
+void solve() {
+	int n; cin >> n;
+    
+    stack<pair<int, int>> st;
+
+    for(int i = 0; i < n; i++) {
+        int x; cin >> x;
+        while(st.size() > 0 && st.top().first >= x) {
+            st.pop();
+        }
+        if(st.size() == 0) cout << 0 << " ";
+        else cout << st.top().second << " ";
+
+        st.push(make_pair(x, i + 1));
+    }   
+
+    cout << '\n';
+}
+
+int main() {
+    fastio;
+    int t = 1;
+    //cin >> t;
+    while(t--) {
+	    solve();
+    }
+
+    return 0;
+}
+
