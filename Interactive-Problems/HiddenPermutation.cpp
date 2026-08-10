5263c366e6ebce2358671192bec728cd68cfd87d
vktalws <victorfalvess22@gmail.com>
2026-08-10 15:01:25 -0300

    feat(tree): tree diameter

---
+++ Interactive-Problems/HiddenPermutation.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define vi vector<int>
+#define vll vector<ll>
+#define vpii vector<pii>
+#define vpll vector<pll>
+
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((int)(x).size())
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+
+#define rep(i, a, b) for (int i = (a); i < (b); i++)
+#define rep0(i, a) rep(i, 0, a)
+
+#define fastio ios_base::sync_with_stdio(false), cin.tie(nullptr)
+
+void solve() {
+	int n; cin >> n;
+    vi nums(n + 1, 0);
+    iota(all(nums), 0);
+
+
+}
+
+int main() {
+    fastio;
+    int t = 1;
+    //cin >> t;
+    while(t--) {
+	    solve();
+    }
+
+    return 0;
+}
+
