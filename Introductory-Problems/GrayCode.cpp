6094da96567ea98669aee806a4c7091eb5ed9ef7
vktalws <victorfalvess22@gmail.com>
2026-08-10 23:40:30 -0300

    feat(introductory): gray code

---
+++ Introductory-Problems/GrayCode.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define vi vector<int>
+#define vll vector<ll>
+#define vpii vector<pii>
+#define vpll vector<pll>
+
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((int)(x).size())
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+
+#define rep(i, a, b) for (int i = (a); i < (b); i++)
+#define rep0(i, a) rep(i, 0, a)
+
+#define fastio ios_base::sync_with_stdio(false), cin.tie(nullptr)
+
+void solve() {
+	int n; cin >> n;
+    for(int i =0; i < (1 << n); i++) {
+        int st = i ^ (i >> 1);
+
+        for(int idx = n - 1; idx >= 0; idx--) cout << ((st >> idx) & 1);
+        cout << '\n';
+    }
+}
+
+int main() {
+    fastio;
+    int t = 1;
+    //cin >> t;
+    while(t--) {
+	    solve();
+    }
+
+    return 0;
+}
+
